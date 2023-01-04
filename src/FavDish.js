75823bc9396388e74863d8fae945ddbc147a2fd7
Reece Vela <reecevela@outlook.com>
2023-01-04 01:15:34 -0600

    Add Fav Dish

--- src/FavDish.js
+++ src/FavDish.js
@@ -1,14 +1,15 @@
-export default class Dish {
+import Dish from "./Dish";
+
+export default class FavDish extends Dish {
   constructor (name, desc) {
-    this.name = name;
-    this.desc = desc;
+    super(name, desc)
   }
 
   appendTo(root){
     const div = document.createElement('div');
     const name = document.createElement('h2');
     const desc = document.createElement('p');
-    div.className = "dish"
+    div.className = "dish fav"
     name.textContent = this.name;
     desc.textContent = this.desc;
     div.appendChild(name);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import data from './data.json';
 import Restaurant from './Restaurant';
 import Dish from './Dish';
+import FavDish from './FavDish';
 
 const root = document.getElementById('content');
 
 data.restaurants.forEach(restaurant => {
   const place = new Restaurant(restaurant.name);
   place.appendTo(root);
+  let favChosen = false;
 
   restaurant.dishes.forEach( dish => {
-    const plate = new Dish(dish.name, dish.description);
+    const plate = (favChosen) ?
+      new Dish(dish.name, dish.description) :
+      new FavDish(dish.name, dish.description);
+    favChosen = true;
     plate.appendTo(root);
   })
 });
